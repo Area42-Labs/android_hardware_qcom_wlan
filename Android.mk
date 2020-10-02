0c0938c42419a8e1279503e7627a9627896d25c9
Palakurthi Sreekanth <sreekfreak995@gmail.com>
2020-10-02 23:51:44 +0530

    YOU JUST SAW NOTHING..... AIGHT

--- Android.mk
+++ Android.mk
@@ -1,4 +1,8 @@
-# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
-ifneq ($(BOARD_IS_AUTOMOTIVE),true)
-include $(call all-subdir-makefiles)
+ifeq ($(call my-dir),$(call project-path-for,qcom-caf_wlan))
+
+ # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
+ ifneq ($(BOARD_IS_AUTOMOTIVE),true)
+ include $(call all-subdir-makefiles)
+ endif
+
 endif
